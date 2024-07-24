fda6d18831495265f810bdda8e5152f568377891
Henrique <henriquemoreira10fk@gmail.com>
2024-07-24 12:06:46 -0300

    Add main function to main file

--- src/syncgroove.py
+++ src/syncgroove.py
@@ -1,18 +1,24 @@
 # Built-in imports
-from sys import exit
 from typing import *
 
 # Local imports
 from utils.config import Config as MainConfig
 from utils.functions import get_youtube_video_data_from_query, get_youtube_video_data_from_url
-from utils.general import set_terminal_title, is_valid_url, clear_terminal
+from utils.general import set_terminal_title, is_valid_url, clear_terminal, make_dirs, download_latest_ffmpeg, open_windows_filedialog_selector
 
 
-set_terminal_title(MainConfig.fancy_name + ' ' + MainConfig.version, MainConfig.os_name)
-print(MainConfig.temporary_path)
+def main() -> None:
+    """
+    The main function of the program.
+    """
+    
+    # Set the terminal title
+    set_terminal_title(MainConfig.fancy_name + ' ' + MainConfig.version, MainConfig.os_name)
+
+    # Create the required directories
+    make_dirs(MainConfig.temporary_app_path)
+    make_dirs(MainConfig.app_path)
 
 
 if __name__ == '__main__':
-    print(get_youtube_video_data_from_query('never gonna give you up'))
-    clear_terminal(MainConfig.os_name)
-    exit()
+    main()
