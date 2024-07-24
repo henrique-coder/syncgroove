03b88441374762016133a8b659757fa60406a06b
Henrique <henriquemoreira10fk@gmail.com>
2024-07-24 12:58:18 -0300

    Optimized imports and other bug fixes

--- src/syncgroove.py
+++ src/syncgroove.py
@@ -2,22 +2,45 @@
 from typing import *
 
 # Local imports
-from utils.config import Config as MainConfig
-from utils.functions import get_youtube_video_data_from_query, get_youtube_video_data_from_url
-from utils.general import set_terminal_title, is_valid_url, clear_terminal, make_dirs, download_latest_ffmpeg, open_windows_filedialog_selector
+from utils.config import Config
+from utils.functions import (
+    get_youtube_video_data_from_query,
+    get_youtube_video_data_from_url
+)
+from utils.general import (
+    init_colorama,
+    ColoredTerminalText as Color,
+    CustomBracket as Bracket,
+    set_terminal_title,
+    is_valid_url,
+    clear_terminal,
+    make_dirs,
+    download_latest_ffmpeg,
+    open_windows_filedialog_selector
+)
 
 
 def main() -> None:
     """
     The main function of the program.
     """
-    
+
+    # print(open_windows_filedialog_selector('Select a file with the URLs/Queries (one by line)', [('Text files', '*.txt'), ('All files', '*.*')]))
+
     # Set the terminal title
-    set_terminal_title(MainConfig.fancy_name + ' ' + MainConfig.version, MainConfig.os_name)
+    set_terminal_title(Config.fancy_name + ' ' + Config.version)
 
     # Create the required directories
-    make_dirs(MainConfig.temporary_app_path)
-    make_dirs(MainConfig.app_path)
+    make_dirs(Config.temporary_path)
+    make_dirs(Config.main_path)
+    make_dirs(Config.default_downloaded_musics_path)
+    make_dirs(Config.main_utils_path)
+    make_dirs(Config.media_path)
+    make_dirs(Config.tools_path)
+
+    # Initialize Colorama for colored terminal output
+    clear_terminal()
+    init_colorama()
 
 
 if __name__ == '__main__':
