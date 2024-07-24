595195fc22f98a55b3c0bebf0559915662b7f0eb
Henrique <henriquemoreira10fk@gmail.com>
2024-07-24 11:30:14 -0300

    Fix FFmpeg downloader and add make_dirs function

--- src/utils/general.py
+++ src/utils/general.py
@@ -2,15 +2,17 @@
 from ctypes import windll, WinError
 from os import PathLike
 from pathlib import Path
+from shutil import rmtree, move
 from subprocess import run, SubprocessError
 from typing import *
-from zipfile import ZipFile
-from shutil import rmtree
 
 # Third-party imports
 from httpx import get, head, HTTPError
 from remotezip import RemoteZip
 from validators import url as is_url, ValidationError
+
+# Local imports
+from .config import Config as MainConfig
 
 
 def set_terminal_title(title: AnyStr, os_name: str) -> None:
@@ -66,36 +68,52 @@ def clear_terminal(os_name: str) -> None:
     except (OSError, SubprocessError):
         raise Exception('Failed to clear terminal.')
 
-def make_dirs(base_path: Union[AnyStr, Path, PathLike], path_list: List[Union[AnyStr, Path, PathLike]]) -> None:
+def make_dirs(base_path: Union[AnyStr, Path, PathLike], path_list: List[Union[AnyStr, Path, PathLike]] = None) -> None:
     """
-    Create subdirectories in a base directory.
+    Create directories in the specified path or create a single directory.
     :param base_path: The base path to create the directories in.
     :param path_list: The list of directories to create.
     """
 
     try:
-        for directory in path_list:
-            Path(base_path, directory).mkdir(parents=True, exist_ok=True)
+        if path_list is None:
+            Path(base_path).mkdir(parents=True, exist_ok=True)
+        else:
+            for directory in path_list:
+                Path(base_path, directory).mkdir(parents=True, exist_ok=True)
     except (FileExistsError, FileNotFoundError):
         raise Exception('Failed to create directories.')
 
-def download_latest_ffmpeg(filepath: Path) -> None:
+def download_latest_ffmpeg(filepath: Union[AnyStr, Path, PathLike], os_name: str) -> None:
     """
     Download the binary of the latest FFmpeg build from the gh@GyanD/codexffmpeg repository.
-    :param filepath: The path to save the FFmpeg binary to.
+    :param filepath: The output path + filename of the FFmpeg binary.
+    :param os_name: The name of the operating system.
     """
 
-    gh_repo_owner = 'GyanD'
-    gh_repo_name = 'codexffmpeg'
+    if os_name == 'windows':
+        gh_repo_owner = 'GyanD'
+        gh_repo_name = 'codexffmpeg'
 
-    api_url = f'https://api.github.com/repos/{gh_repo_owner}/{gh_repo_name}/releases/latest'
-    gh_data = get(api_url).json()
+        api_url = f'https://api.github.com/repos/{gh_repo_owner}/{gh_repo_name}/releases/latest'
+        try: response = get(api_url)
+        except HTTPError: raise Exception('Failed to fetch the latest release data.')
 
-    github_repository = f'https://github.com/{github_repository_owner}/{github_repository_name}'
-    build_name = f'ffmpeg-{gh_data['tag_name']}-essentials_build'
+        if not response.is_success:
+            raise Exception('Failed to fetch the latest release data.')
 
-    with RemoteZip(f'{github_repository}/releases/latest/download/{build_name}.zip') as rzip:
-        rzip.extract(f'{build_name}/bin/{file_name}.exe', output_file_dir)
+        response_data = response.json()
 
-        Path(f'{output_file_dir}/{build_name}/bin/{file_name}.exe').rename(Path(f'{output_file_dir}/{file_name}.exe'))
-        rmtree(Path(f'{output_file_dir}/{build_name}'), ignore_errors=True)
+        github_repository = f'https://github.com/{gh_repo_owner}/{gh_repo_name}'
+        build_name = f'ffmpeg-{response_data['tag_name']}-essentials_build'
+        temporary_ffmpeg_path = Path(MainConfig.temporary_path + '/.downloaded_ffmpeg')
+
+        try:
+            with RemoteZip(f'{github_repository}/releases/latest/download/{build_name}.zip') as r_zip:
+                r_zip.extract(f'{build_name}/bin/ffmpeg.exe', temporary_ffmpeg_path)
+                move(Path(temporary_ffmpeg_path, f'{build_name}/bin/ffmpeg.exe'), Path(filepath).resolve())
+                rmtree(temporary_ffmpeg_path)
+        except Exception:
+            raise Exception('Failed to download the latest FFmpeg build.')
+    else:
+        raise Exception('FFmpeg download is only supported on Windows. (for now)')
