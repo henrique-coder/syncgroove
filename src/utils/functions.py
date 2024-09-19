d777e18eeb7658ae5ee76ac910f4e92ff2755095
Henrique <henriquemoreira10fk@gmail.com>
2024-09-19 14:19:05 -0300

    Removed old data extractor (external api)

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -13,15 +13,7 @@ def get_youtube_video_data_from_query(query: str) -> Dict[str, Any]:
     :raises Exception: If the server is down or if the request fails.
     """
 
-    try:
-        response = get(url='https://private-b9a5ee-everytoolsapi.apiary-mock.com/api/v2/get-youtube-video-url-from-search', params={'query': query}, timeout=30)
-    except HTTPError:
-        raise Exception('Failed to get the YouTube video URL from the query. The server is down.')
-
-    if not response.is_success:
-        raise Exception(f'Failed to get the YouTube video URL from the query. Status code: {response.status_code} - Response: {response.text}')
-
-    return dict(response.json()['response']['foundUrlData'])
+    pass
 
 
 def get_youtube_video_data_from_url(url: str) -> Dict[str, Any]:
