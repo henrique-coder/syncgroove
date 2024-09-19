85e19fe9a369ef397de1141c7f44191476b5c02a
Henrique <henriquemoreira10fk@gmail.com>
2024-09-19 18:40:01 -0300

    Removed is_macos and is_java vars from config class and renamed main resources path name

--- src/utils/config.py
+++ src/utils/config.py
@@ -8,14 +8,12 @@ class Config:
     os_name = system_name().lower() or '?'
     is_windows = bool(os_name == 'windows')
     is_linux = bool(os_name == 'linux')
-    is_macos = bool(os_name == 'darwin')
-    is_java = bool(os_name == 'java')
     version = '0.0.1'
     name = 'syncgroove'
     fancy_name = 'SyncGroove'
     temporary_path = Path((gettempdir() or 'tmp'), f'{name}-{version}').resolve().as_posix()
     main_path = Path(Path.cwd(), f'{name}-{version}').resolve().as_posix()
     default_downloaded_musics_path = Path(main_path, 'downloaded-musics').resolve().as_posix()
-    main_utils_path = Path(main_path, '.utils').resolve().as_posix()
-    media_path = Path(main_utils_path, 'media').resolve().as_posix()
-    tools_path = Path(main_utils_path, 'tools').resolve().as_posix()
+    main_resources_path = Path(main_path, 'resources').resolve().as_posix()
+    media_path = Path(main_resources_path, 'media').resolve().as_posix()
+    tools_path = Path(main_resources_path, 'tools').resolve().as_posix()
