1a2dd08fa0262e913a52ad84e8b879b2a3aee693
Henrique <henriquemoreira10fk@gmail.com>
2024-09-20 20:59:42 -0300

    Added regexes for urls (separator) and removed unused functions

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,27 +1,57 @@
-# Built-in imports
-from typing import Any, Dict
+from typing import *
+from re import match as re_match
+from os import PathLike
 
-# Third-party imports
-from httpx import get, HTTPError
+from pysmartdl2 import SmartDL
 
 
-def get_youtube_video_data_from_query(query: str) -> Dict[str, Any]:
+def sort_urls_by_type_and_domain(input_queries_obj: type) -> type:
     """
-    Get the YouTube video data from a query.
-    :param query: The query to search for the video.
-    :return: The YouTube video data from the query.
-    :raises Exception: If the server is down or if the request fails.
-    """
-
-    pass
-
-
-def get_youtube_video_data_from_url(url: str) -> Dict[str, Any]:
-    """
-    Get the YouTube video data from a URL.
-    :param url: The URL of the video.
-    :return: The YouTube video data from the URL.
-    :raises Exception: If the server is down or if the request fails.
+    Sort URLs by type (single or playlist) and domain.
+    :param input_queries_obj: The InputQueries object.
+    :return: The updated InputQueries object.
     """
 
-    pass
+    def classify_urls(url: str, obj: Dict[str, Any]) -> None:
+        if re_match(obj['regexes']['playlist'], url):
+            obj['urls']['playlist'].append(url)
+        elif re_match(obj['regexes']['single'], url):
+            obj['urls']['single'].append(url)
+
+    youtube_obj = {
+        'fancyName': 'YouTube',
+        'urls': {'single': [], 'playlist': []},
+        'regexes': {
+            'single': r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w-]+(&[^\s]*)?$',
+            'playlist': r'(https?://)?(www\.)?youtube\.com/(watch\?v=[\w-]+&list=|playlist\?list=)[\w-]+'
+        }
+    }
+
+    youtube_music_obj = {
+        'fancyName': 'YouTube Music',
+        'urls': {'single': [], 'playlist': []},
+        'regexes': {
+            'single': r'(https?://)?(www\.)?music\.youtube\.com/watch\?v=[\w-]+(&[^\s]*)?$',
+            'playlist': r'(https?://)?(www\.)?music\.youtube\.com/playlist\?list=[\w-]+'
+        }
+    }
+
+    for url in input_queries_obj._urls:
+        classify_urls(url, youtube_obj)  # YouTube
+        classify_urls(url, youtube_music_obj)  # YouTube Music
+
+    # Update the InputQueries object with the sorted URLs
+    input_queries_obj.SortedURLs.youtube = youtube_obj
+    input_queries_obj.SortedURLs.youtube_music = youtube_music_obj
+
+    return input_queries_obj
+
+def download_file(url: str, output_path: PathLike) -> None:
+    """
+    Download a file from the internet.
+    :param url: The URL of the file.
+    :param output_path: The path where the file will be saved.
+    """
+
+    download_obj = SmartDL(url, output_path)
+    download_obj.start()
