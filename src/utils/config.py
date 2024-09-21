6dd2f53f9c647339cee352c04798dd0d8713c536
Henrique <henriquemoreira10fk@gmail.com>
2024-09-21 16:16:13 -0300

    Added typings for config variables

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,15 +5,15 @@ from platform import system as system_name
 
 
 class Config:
-    os_name = system_name().lower() or '?'
-    is_windows = bool(os_name == 'windows')
-    is_linux = bool(os_name == 'linux')
-    version = '0.0.1'
-    name = 'syncgroove'
-    fancy_name = 'SyncGroove'
-    temporary_path = Path((gettempdir() or 'tmp'), f'{name}-{version}').resolve().as_posix()
-    main_path = Path(Path.cwd(), f'{name}-{version}').resolve().as_posix()
-    default_downloaded_musics_path = Path(main_path, 'downloaded-musics').resolve().as_posix()
-    main_resources_path = Path(main_path, 'resources').resolve().as_posix()
-    media_path = Path(main_resources_path, 'media').resolve().as_posix()
-    tools_path = Path(main_resources_path, 'tools').resolve().as_posix()
+    os_name: str = system_name().lower() or '?'
+    is_windows: bool = bool(os_name == 'windows')
+    is_linux: bool = bool(os_name == 'linux')
+    version: str = '0.0.1'
+    name: str = 'syncgroove'
+    fancy_name: str = 'SyncGroove'
+    temporary_path: str = Path((gettempdir() or 'tmp'), f'{name}-{version}').resolve().as_posix()
+    main_path: str = Path(Path.cwd(), f'{name}-{version}').resolve().as_posix()
+    default_downloaded_musics_path: str = Path(main_path, 'downloaded-musics').resolve().as_posix()
+    main_resources_path: str = Path(main_path, 'resources').resolve().as_posix()
+    media_path: str = Path(main_resources_path, 'media').resolve().as_posix()
+    tools_path: str = Path(main_resources_path, 'tools').resolve().as_posix()
