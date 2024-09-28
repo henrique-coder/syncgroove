a92e9b2a744e59b49d3dc27214ed2e6686f9db1a
Henrique <henriquemoreira10fk@gmail.com>
2024-09-28 16:08:52 -0300

    Changed default downloded musics directory

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,6 +3,9 @@ from tempfile import gettempdir
 from pathlib import Path
 from platform import system as system_name
 
+# Third-party imports
+from platformdirs import user_music_dir, user_downloads_dir
+
 
 class Config:
     os_name: str = system_name().lower() or '?'
@@ -13,7 +16,7 @@ class Config:
     fancy_name: str = 'SyncGroove'
     temporary_path: str = Path((gettempdir() or 'tmp'), f'{name}-{version}').resolve().as_posix()
     main_path: str = Path(Path.cwd(), f'{name}-{version}').resolve().as_posix()
-    default_downloaded_musics_path: str = Path(main_path, 'downloaded-musics').resolve().as_posix()
+    default_downloaded_musics_path: str = user_music_dir() or user_downloads_dir() or Path.home().resolve().as_posix()
     main_resources_path: str = Path(main_path, 'resources').resolve().as_posix()
     media_path: str = Path(main_resources_path, 'media').resolve().as_posix()
     tools_path: str = Path(main_resources_path, 'tools').resolve().as_posix()
